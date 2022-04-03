a148f5c3b17b9ceffbb1a36dd68d5f79720fb7e2
Ryu <78216411+dongmen5149@users.noreply.github.com>
2022-04-03 11:09:37 +0900

    문제풀이

--- 1week/동규/3_Largest Number.js
+++ 1week/동규/3_Largest Number.js
@@ -1,10 +1,10 @@
-var largestNumber = function (nums) {
+function largestNumber(nums) {
   return (
     nums
       .sort((a, b) => "" + b + a - ("" + a + b))
       .join("")
       .replace(/^0*/g, "") || "0"
   );
-};
+}
 
 console.log(largestNumber([10, 2]));
