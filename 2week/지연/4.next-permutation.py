9ecab680bfdbeddd9671f2618f8d48c3b9f02e50
gparkkii <jyp933@gmail.com>
2022-04-15 18:21:52 +0900

    week 2 minor changes

--- 2week/지연/4.next-permutation.py
+++ 2week/지연/4.next-permutation.py
@@ -1,9 +1,11 @@
 from typing import List
 
+# 순열 찾기
+
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        end_idx = max_idx = len(nums) - 1  # 4
+        end_idx = max_idx = len(nums) - 1
 
         # 큰 수 위치 찾기
         while max_idx > 0 and nums[max_idx - 1] >= nums[max_idx]:
@@ -15,12 +17,13 @@ class Solution:
             return nums
 
         # find the last "ascending" position
-        min_idx = max_idx - 1
-        while nums[end_idx] <= nums[min_idx]:
+        swap_idx = max_idx - 1
+        while nums[end_idx] <= nums[swap_idx]:
             end_idx -= 1
-        nums[min_idx], nums[end_idx] = nums[end_idx], nums[min_idx]
+        nums[swap_idx], nums[end_idx] = nums[end_idx], nums[swap_idx]
+
         # reverse the second part
-        max, min = min_idx + 1, len(nums) - 1
+        max, min = swap_idx + 1, len(nums) - 1
         while max < min:
             nums[max], nums[min] = nums[min], nums[max]
             max += 1
