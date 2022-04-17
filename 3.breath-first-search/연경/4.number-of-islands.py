cace3ad77c7c35b5736fdd26709383dec76d3854
yeonkyungJoo <younkyong94@gmail.com>
2022-04-17 11:37:19 +0900

    2022.04.17

--- 3.breath-first-search/연경/4.number-of-islands.py
+++ 3.breath-first-search/연경/4.number-of-islands.py
@@ -1,5 +1,36 @@
 from collections import deque
 from typing import List
+
+'''
+class Solution:
+    
+    def dfs(self, grid: List[List[str]], i, j):
+
+        if i < 0 or i >= len(grid) \
+          or j < 0 or j >= len(grid[0]) \
+              or grid[i][j] != "1" :
+          return
+
+        grid[i][j] = 0
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+          return 0
+
+        count = 0
+        for i in range(len(grid)):
+          for j in range(len(grid[0])):
+            if grid[i][j] == "1":
+              self.dfs(grid, i, j)
+              count += 1
+
+        return count
+'''
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
