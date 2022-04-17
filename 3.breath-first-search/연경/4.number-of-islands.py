b204b66c55b97bb795433f1b8e8ce2303e55b0db
yeonkyungJoo <younkyong94@gmail.com>
2022-04-17 11:06:57 +0900

    2022.04.17 (2, 3, 4)

---
+++ 3.breath-first-search/연경/4.number-of-islands.py
@@ -0,0 +1,37 @@
+from collections import deque
+from typing import List
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        dx = [-1, 0, 1, 0]
+        dy = [0, -1, 0, 1]
+        # row(y), col(x)
+        row, col = len(grid), len(grid[0])
+        count = 0
+        visited = [[False for _ in range(col)] for _ in range(row)]
+        for i in range(row):
+            for j in range(col):
+                if visited[i][j]:
+                    continue
+                if grid[i][j] == '0':
+                    continue
+                count += 1
+                visited[i][j] = True
+                queue = deque([(i, j)])
+                while queue:
+                    y, x = queue.popleft()
+                    for k in range(4):
+                        ny, nx = y + dy[k], x + dx[k]
+                        if 0 <= nx < col and 0 <= ny < row and grid[ny][nx] == '1' and not visited[ny][nx]:
+                            visited[ny][nx] = True
+                            queue.append((ny, nx))
+        return count
+
+if __name__ == "__main__":
+    grid = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]
+    ]
+    print(Solution().numIslands(grid))
