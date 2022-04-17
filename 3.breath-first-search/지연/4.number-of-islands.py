99b5a9f341c6b931985f6747b991ac16a762d4a9
gparkkii <jyp933@gmail.com>
2022-04-17 10:58:02 +0900

    week4 jiyeon

---
+++ 3.breath-first-search/지연/4.number-of-islands.py
@@ -0,0 +1,45 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        LAND = '1'
+        ROW_IDX = len(grid)
+        COL_IDX = len(grid[0])
+
+        visited = [[False for _ in range(COL_IDX)]for _ in range(ROW_IDX)]
+
+        DY = [0, 0, 1, -1]
+        DX = [1, -1, 0, 0]
+        land_nums = 0
+
+        for row in range(ROW_IDX):
+            for col in range(COL_IDX):
+
+                if(grid[row][col] == LAND and visited[row][col] == False):
+                    visited[row][col] = True
+                    que = deque([(row, col)])
+                    land_nums += 1
+
+                    while que:
+                        y, x = que.popleft()
+                        for i in range(4):
+                            ny, nx = y + DY[i], x + DX[i]
+                            if (
+                                0 <= ny < ROW_IDX and
+                                0 <= nx < COL_IDX and
+                                visited[ny][nx] == False and
+                                grid[ny][nx] == LAND
+                            ):
+                                que.append((ny, nx))
+                                visited[ny][nx] = True
+        return land_nums
+
+
+print(Solution().numIslands([
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]))
