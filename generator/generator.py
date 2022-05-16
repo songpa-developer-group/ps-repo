c5a3b627ea23665596bc17eb9eafead9282721e0
geunwoo Baek <geunu3751@nexon.co.kr>
2022-05-16 17:35:02 +0900

    update generator.py

--- generator/generator.py
+++ generator/generator.py
@@ -2,47 +2,49 @@ import argparse
 import os
 import shutil
 
-CURRENT_PATH = os.path.dirname(__file__)
-TEMPLATE_PATH = "{}/template".format(CURRENT_PATH)
-PROBLEMLIST_PATH = "{}/problemlist.md".format(CURRENT_PATH)
+ROOT_PATH = os.path.dirname(__file__)
 
-
-def get_subject_and_urls_from_file(file_name):
-    inputs = open("{}/{}".format(CURRENT_PATH, file_name), "r")
+def get_subject_and_urls(file_name):
+    inputs = open("{}/{}".format(ROOT_PATH, file_name), "r")
     lines = [line.replace("\n", "") for line in inputs.readlines()]
     inputs.close()
     return lines[0], lines[1:]
 
 
-def create_problem_list_from_subject_and_urls(subject, urls):
-    problemlist = open(PROBLEMLIST_PATH, "w+")
+def create_problem_list(problem_list_path, urls, url_format):
+    if os.path.exists(problem_list_path):
+        shutil.rmtree(problem_list_path, ignore_errors=True)
+    os.mkdir(problem_list_path)
+
+    problemlist = open("{}/{}".format(problem_list_path,"problemlist.md"), "w+")
     problemlist.write("# {}\n".format(subject))
     for num, url in enumerate(urls):
         problemlist.writelines(
             "## {num}. [{name}]({url})\n".format(
-                num=num + 1, name=_parse_name_from_url(url), url=url
+                num=num + 1, name=_parse_name_from_url(url,url_format), url=url
             )
         )
     problemlist.close()
 
 
-def create_template_from_urls(urls):
-    if os.path.exists(TEMPLATE_PATH):
-        shutil.rmtree(TEMPLATE_PATH, ignore_errors=True)
-    os.mkdir(TEMPLATE_PATH)
+def create_template(template_path, urls, url_format, file_type):
+    if os.path.exists(template_path):
+        shutil.rmtree(template_path, ignore_errors=True)
+    os.mkdir(template_path)
+
     for num, url in enumerate(urls):
         p_name = (
             "{}.".format(num + 1)
-            + _parse_name_from_url(url)
-            + ".{}".format(FILE_TYPE)
+            + _parse_name_from_url(url,url_format)
+            + ".{}".format(file_type)
         )
-        p_file = open("{}/{}".format(TEMPLATE_PATH, p_name), "w+")
+        p_file = open("{}/{}".format(template_path, p_name), "w+")
         p_file.write("## {}".format(url))
         p_file.close()
 
 
-def _parse_name_from_url(url):
-    return "".join(url.split(URL_FORMAT))[:-1]
+def _parse_name_from_url(url, url_format):
+    return "".join(url.split(url_format))[:-1]
 
 
 if __name__ == "__main__":
@@ -72,9 +74,20 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    URL_FORMAT = args.problem_origin_url
-    FILE_TYPE = args.file_type
-    INPUT_FILE_NAME = args.input_file
-    subject, urls = get_subject_and_urls_from_file(INPUT_FILE_NAME)
-    create_problem_list_from_subject_and_urls(subject, urls)
-    create_template_from_urls(urls)
+    url_format = args.problem_origin_url
+    file_type = args.file_type
+    subject, urls = get_subject_and_urls(args.input_file)
+    cur_path = "{}/{}".format(ROOT_PATH, subject)
+
+    create_problem_list(
+        problem_list_path = cur_path,
+        urls=urls,
+        url_format = url_format
+    )
+
+    create_template(
+        template_path = "{}/template".format(cur_path),
+        urls = urls,
+        url_format = url_format,
+        file_type = args.file_type
+    )
